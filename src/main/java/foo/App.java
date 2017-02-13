39391f7d53ac5765dc45e8403e207fe1fbe7b2e5
meiguoliang@126.com <meiguoliang@jd.com>
2017-02-13 18:37:00 +0800

    学习
    学习项目

---
+++ src/main/java/foo/App.java
@@ -0,0 +1,16 @@
+package foo;
+
+import java.util.ArrayList;
+
+import java.util.List;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+    	List a = new ArrayList();
+        System.out.println( "Hello World!" );
+    }
+}
