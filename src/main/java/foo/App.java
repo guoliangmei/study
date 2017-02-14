9e2887c60aa5884d243474c85bd61545ddf60b29
meiguoliang@126.com <meiguoliang@jd.com>
2017-02-14 13:42:19 +0800

    cxvcxvxc

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -12,5 +12,6 @@ public class App
     public static void main( String[] args ){
     	List a = new ArrayList();
         System.out.println( "Hello World!" );
+        System.out.println();
     }
 }
