df3b27a91858e9d082b429ae72a74dd2adbd2651
meiguoliang@126.com <meiguoliang@jd.com>
2017-03-06 16:29:15 +0800

    fvd

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -1,17 +1,33 @@
 package foo;
 
 import java.util.ArrayList;
-
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ){
-    	List a = new ArrayList();
-        System.out.println( "Hello World!" );
-        System.out.println();
+	public static class Task implements Callable<String>{
+
+		public String call() throws Exception {
+			 String tid = String.valueOf(Thread.currentThread().getId());
+	         System.out.printf("Thread#%s : in call\n", tid);
+	         return tid;
+		}
+		
+	}
+    public static void main( String[] args ) throws InterruptedException, ExecutionException{
+    	List<Future<String>> results = new ArrayList<Future<String>>();
+    	ExecutorService es = Executors.newCachedThreadPool();
+    	 for(int i=0; i<100;i++) results.add(es.submit(new Task()));
+    	 for(Future<String> res : results){
+    		 System.out.println(res.get());
+    	 }
     }
 }
