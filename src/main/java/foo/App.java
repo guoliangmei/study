95ed2144ced12cbf7edc641dfcde5625b18741c2
guoliangmei@126.com <meiguoliang@jd.com>
2017-04-26 20:29:49 +0800

    test

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -1,12 +1,13 @@
 package foo;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 /**
  * Hello world!
  *
@@ -23,13 +24,31 @@ public class App
 		
 	}
     public static void main( String[] args ) throws InterruptedException, ExecutionException{
-    	List<Future<String>> results = new ArrayList<Future<String>>();
-    	ExecutorService es = Executors.newCachedThreadPool();
-    	 for(int i=0; i<100;i++) results.add(es.submit(new Task()));
-    	 for(Future<String> res : results){
-    		 System.out.println(res.get());
-    	 }
-    	 System.out.println("11111111111111");
-    	 System.out.println("222222222222222222222");
+//      ByteBuffer b = ByteBuffer.allocate(15);
+//      System.out.println("limit=" + b.limit() + " capacity=" + b.capacity() + " position=" + b.position());
+//      for(int i=0;i<10;i++){
+//    	  b.put((byte) i);
+//      }
+//      System.out.println("limit=" + b.limit() + " capacity=" + b.capacity() + " position=" + b.position());
+//      b.flip(); // 重置position
+//      System.out.println("limit=" + b.limit() + " capacity=" + b.capacity() + " position=" + b.position());
+//    	String str = "Test string";
+//    	StringBuilder strBuilder = new StringBuilder(str);
+//    	strBuilder.setCharAt(1, 'X');
+//    	str=strBuilder.toString();
+//    	System.out.println(str);
+//    	Integer a = 1270;
+//    	Integer b = 1270;
+//    	int c = 1270;
+//    	System.out.println(a == b);
+//    	System.out.println(a.equals(b));
+//    	System.out.println(a.equals(c));
+//    	char a = '9';
+//    	System.out.println(Character.getNumericValue(a));
+    	
+    	int a = 202;
+    	int b = 202;
+    	System.out.println(a==b);
+    	
     }
 }
