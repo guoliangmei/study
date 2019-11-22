cdc8995af874df58cb9c68459c7eea69a2ecef2c
michael <guoliangmei@126.com>
2019-11-22 20:43:27 +0800

    sadfsadf

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -11,8 +11,8 @@ public class App
 	// mybatis生成工具
 	// https://www.jianshu.com/p/2ec9337dc2b0
     public static void main( String[] args ) throws UnknownHostException{
-//    	Vector v=new Vector();  
-//    	for(int i=0;i<25;i++) v.add(new byte[1*1024*1024]);
+   	Vector v=new Vector();
+    	for(int i=0;i<25;i++) v.add(new byte[1*1024*1024]);
     	long l = 10000L;
     	System.out.println(String.valueOf(l));
     	
